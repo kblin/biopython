03a9ca83dd725749dfd5c6c0f81cd3ace706c445
bow <bow@bow.web.id>
2012-08-01 21:44:36 +0200

    Fix HSPFragment __str__ displays

--- Bio/SearchIO/_objects.py
+++ Bio/SearchIO/_objects.py
@@ -1381,7 +1381,7 @@ class HSPFragment(BaseHSP):
             try:
                 hseq = str(self.hit.seq)
             except AttributeError:  # hit is None
-                qseq = '?'
+                hseq = '?'
 
             # homology line
             homol = ''
@@ -1473,9 +1473,9 @@ class HSPFragment(BaseHSP):
         # alignment span can be its own attribute, or computed from
         # query / hit length
         if not hasattr(self, '_aln_len'):
-            if hasattr(self, 'query'):
+            if self.query is not None:
                 self._aln_len = len(self.query)
-            elif hasattr(self, 'hit'):
+            elif self.hit is not None:
                 self._aln_len = len(self.hit)
             else:
                 self._aln_len = None
